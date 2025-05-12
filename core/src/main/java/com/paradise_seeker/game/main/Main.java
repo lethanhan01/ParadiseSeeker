8f006f9e4eb6d5801b26063d9078ea635155353f
hiepNH272 <hiep.nh235708@sis.hust.edu.vn>
2025-05-13 00:22:50 +0700

    Update main

--- core/src/main/java/com/paradise_seeker/game/main/Main.java
+++ core/src/main/java/com/paradise_seeker/game/main/Main.java
@@ -1,11 +1,39 @@
 package com.paradise_seeker.game.main;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+
 public class Main extends Game {
-    @Override
-    public void create() {
-        setScreen(new FirstScreen());
-    }
+
+	public SpriteBatch batch;
+	public BitmapFont font;
+	public FitViewport viewport;
+
+
+	public void create() {
+		batch = new SpriteBatch();
+		// use libGDX's default font
+		font = new BitmapFont();
+		viewport = new FitViewport(16,10);
+		
+		//font has 15pt, but we need to scale it to our viewport by ratio of viewport height to screen height 
+		font.setUseIntegerPositions(false);
+		font.getData().setScale(viewport.getWorldHeight() / Gdx.graphics.getHeight());
+		
+		this.setScreen(new MainMenuScreen(this));
+	}
+
+	public void render() {
+		super.render(); // important!
+	}
+
+	public void dispose() {
+		batch.dispose();
+		font.dispose();
+	}
+
 }

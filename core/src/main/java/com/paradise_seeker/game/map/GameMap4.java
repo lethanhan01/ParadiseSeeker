802a98fcae8aefac0a39f373731532d3f95d57fe
nhithedev <nhitofu@gmail.com>
2025-05-30 22:15:23 +0700

    added map 4

--- core/src/main/java/com/paradise_seeker/game/map/GameMap4.java
+++ core/src/main/java/com/paradise_seeker/game/map/GameMap4.java
@@ -3,10 +3,10 @@ package com.paradise_seeker.game.map;
 import com.paradise_seeker.game.entity.object.Chest;
 import com.paradise_seeker.game.entity.object.item.Fragment;
 
-public class GameMap1 extends GameMap {
-    public GameMap1() {
+public class GameMap4 extends GameMap {
+    public GameMap4() {
         super();
-        this.mapName = "Forgotten Ruins";
+        this.mapName = "Old Castle";
         // If you have portals, chest, etc, add them here!
         // Example:
         // this.portal = new Portal(15f, 25f);	
@@ -14,11 +14,11 @@ public class GameMap1 extends GameMap {
 
     @Override
     protected String getMapTmxPath() {
-        return "tilemaps/TileMaps/maps/map1.tmx";
+        return "tilemaps/TileMaps/maps/map4.tmx";
     }
 
     @Override
     protected String getMapBackgroundPath() {
-        return "tilemaps/TileMaps/maps/map1.png";
+        return "tilemaps/TileMaps/maps/map4.png";
     }
 }
